82ef10ee2436f8335f2d3362b1975150b51c00cb
Javier Villegas Gonzalez <javier.villegas.g@usach.cl>
2024-12-07 18:31:17 -0300

    varios avances en la implementacion de kubernetes, todo aun sin probar

--- client-service/src/main/java/com/example/client_service/entity/ClientEntity.java
+++ client-service/src/main/java/com/example/client_service/entity/ClientEntity.java
@@ -1,17 +1,13 @@
 package com.example.client_service.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "client")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
